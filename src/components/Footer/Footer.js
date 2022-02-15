8dd91517011ccecb208a1ad5b88e6f3627540b45
Yuvraj Singh Chauhan <ng3w.uv@gmail.com>
2022-02-15 15:56:34 +0530

    npm running

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,20 +5,15 @@ const Footer = () => {
   return (
     <div className="footer">
       <hr style={{ width: "90%", marginTop: 20 }} />
-      <span className="name">
-        Made by{" "}
-        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
-          Roadside Coder
-        </a>
-      </span>
+      
       <div className="iconContainer">
-        <a href="https://www.instagram.com/roadsidecoder/" target="__blank">
+        <a href="https://www.instagram.com/ng3w_uv/" target="__blank">
           <i className="fab fa-instagram-square fa-2x"></i>
         </a>
-        <a href="https://www.linkedin.com/in/piyush-eon" target="__blank">
+        <a href="https://www.linkedin.com/in/yuvraj-ng3w/" target="__blank">
           <i className="fab fa-linkedin fa-2x"></i>
         </a>
-        <a href="https://www.youtube.com/c/RoadsideCoder" target="__blank">
+        <a href="https://m.youtube.com/channel/UCtnySSItnm51-uNGqDFSmoA?sub_comfirmation=1" target="__blank">
           <i className="fab fa-youtube fa-2x"></i>
         </a>
       </div>
